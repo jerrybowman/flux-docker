f1a375670924b53677950e03296ec762f9ac0f56
Jerry Bowman <jerry.bowman@gmail.com>
2019-03-23 16:43:26 -0500

    Cleanup various testing code

--- local/compose_run.sh
+++ local/compose_run.sh
@@ -4,8 +4,8 @@ if [ ! -z ${RUN_DETACHED} ]; then
 	DETACHED_PARM="-d"
 fi
 
-# We have to dalay the startup of the engine and console longer if the database has not
-# been initialized. But all the user to pick a deley value to override the defaults.
+# We have to delay the startup of the engine and console longer if the database has not
+# been initialized. But also allow the user to pick a delay value to override the defaults.
 
 if [ -z ${DELAY} ]; then
 	if [ -f pgdata/postgresql/.initialized ]; then
